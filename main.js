5b88585cd73455e591a9385730ca811f15b86861
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-02-01 18:36:52 +0530

    Add files via upload
    JS done

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+rightWristX =0;
+leftWristX =0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +17,10 @@ function setup(){
 
 function draw(){
     background('#7adede');
+    document.getElementById("square_side").innerHTML = "Width And Height of a square will be = "+difference + "px";
+    fill("#50de92");
+    stroke("#000000");
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded(){
@@ -21,5 +31,15 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX -" + noseX + "noseY =" +noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("rightWristX =" + rightWristX + "leftWristX =" + leftWristX);
+        difference = floor(leftWristX - rightWristX);
+        console.log(difference);
+
     }
 }
